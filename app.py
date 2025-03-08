d7f57c53fede22953d7d239d8748e7b5dae044f7
sreeja-k04 <162904546+sreeja-k04@users.noreply.github.com>
2025-03-08 14:46:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,24 @@ def main():
     st.write("Enter the feature values for prediction:")
     
     # Example input fields (modify according to your model features)
-    feature1 = st.number_input("Petrol Tax: ", value=0.0)
-    feature2 = st.number_input("Average income", value=0.0)
-    feature3 = st.number_input("Paved Highways", value=0.0)
-    feature4 = st.number_input("Population_Driver_licence(%)", value=0.0)
+    feature1 = st.number_input("Age", value=0.0)
+    feature2 = st.number_input("Sex", value=0.0)
+    feature3 = st.number_input("Chest pain type", value=0.0)
+    feature4 = st.number_input("Blood Pressure (BP)", value=0.0)
+    feature5 = st.number_input("Cholesterol", value=0.0)
+    feature6 = st.number_input("FBS over 120", value=0.0)
+    feature7 = st.number_input("EKG results", value=0.0)
+    feature8 = st.number_input("Max Heart Rate (HR)", value=0.0)
+    feature9 = st.number_input("Exercise-induced angina", value=0.0)
+    feature10 = st.number_input("ST depression", value=0.0)
+    feature11 = st.number_input("Slope of ST", value=0.0)
+    feature12 = st.number_input("Number of vessels fluro", value=0.0)
+    feature13 = st.number_input("Thallium", value=0.0)
     
     model = load_model()
     
     if st.button("Predict"):
-        input_data = np.array([[feature1, feature2, feature3,feature4]])
+        input_data = np.array([[feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13]])
         prediction = model.predict(input_data)
         st.write("Prediction:", prediction)
 
